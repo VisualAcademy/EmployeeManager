a4610619e02f895be40b8658b4edb4d008b15b98
RedPlus <redplus@redplus.net>
2021-02-27 06:52:39 +0900

    SQL Server 데이터베이스 프로젝트에 2개의 테이블 구조 생성

---
+++ EmployeeManager.SqlServer/dbo/Tables/Employees.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Employees] (
+    [Id]         BIGINT         IDENTITY (1, 1) NOT NULL,
+    [FirstName]  NVARCHAR (MAX) NULL,
+    [LastName]   NVARCHAR (MAX) NULL,
+    [MiddleName] NVARCHAR (MAX) NULL,
+    [DOB]        NVARCHAR (MAX) NULL,
+    [SSN]        NVARCHAR (MAX) NULL,
+    [Email]      NVARCHAR (MAX) NULL,
+    [Address]    NVARCHAR (MAX) NULL,
+    [City]       NVARCHAR (MAX) NULL,
+    [State]      NVARCHAR (MAX) NULL,
+    [PostalCode] NVARCHAR (MAX) NULL,
+    [Gender]     NVARCHAR (MAX) NULL,
+    [Active]     BIT            NOT NULL,
+    [CreatedAt]  DATETIME2 (7)  NOT NULL,
+    CONSTRAINT [PK_Employees] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ EmployeeManager.SqlServer/dbo/Tables/EmployeesHistories.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[EmployeesHistories] (
+    [Id]         BIGINT         IDENTITY (1, 1) NOT NULL,
+    [EmployeeId] BIGINT         NOT NULL,
+    [FirstName]  NVARCHAR (MAX) NULL,
+    [LastName]   NVARCHAR (MAX) NULL,
+    [MiddleName] NVARCHAR (MAX) NULL,
+    [DOB]        NVARCHAR (MAX) NULL,
+    [SSN]        NVARCHAR (MAX) NULL,
+    [Email]      NVARCHAR (MAX) NULL,
+    [Address]    NVARCHAR (MAX) NULL,
+    [City]       NVARCHAR (MAX) NULL,
+    [State]      NVARCHAR (MAX) NULL,
+    [PostalCode] NVARCHAR (MAX) NULL,
+    [Gender]     NVARCHAR (MAX) NULL,
+    [Active]     BIT            NOT NULL,
+    [CreatedAt]  DATETIME2 (7)  NOT NULL,
+    [Remarks]    NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_EmployeesHistories] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
